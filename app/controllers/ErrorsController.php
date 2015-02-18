8c37754ffc8f9d2821b16d7f05c28b9b0154d71e
loick111 <loick111@gmail.com>
2015-02-18 21:06:02 +0100

    update

---
+++ app/controllers/ErrorsController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace app\controllers;
+
+
+use SFramework\mvc\Controller;
+
+class ErrorsController extends Controller{
+
+    public function err404()
+    {
+        $this->getView()->render('errors/err404');
+    }
+}
---
+++ app/controllers/HomeController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace app\controllers;
+
+use SFramework\mvc\Controller;
+
+class HomeController extends Controller
+{
+
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    public function index()
+    {
+        $this->getView()->render('home/index');
+    }
+}
