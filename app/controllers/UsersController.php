1c3018e0794fbd3fc42a72a8bc1af052ace211fb
loick111 <loick111@gmail.com>
2015-03-15 21:46:17 +0100

    update

--- app/controllers/UsersController.php
+++ app/controllers/UsersController.php
@@ -24,7 +24,15 @@ class UsersController extends Controller
         if (!Authentication::getInstance()->isAuthenticated())
             $this->getView()->redirect('/users/login');
 
-        $vars = ['username' => $this->getParams()[0]];
+        $vars = [
+            'user' => [
+                'username' => 'loick111',
+                'mail' => 'loick111@gmail.com',
+                'hash' => md5('loick111@gmail.com'),
+                'firstname' => 'Loïck',
+                'lastname' => 'Mahieux'
+            ]
+        ];
 
         $this->getView()->render('users/show', $vars);
     }
@@ -36,9 +44,26 @@ class UsersController extends Controller
         $this->getView()->render('users/login');
     }
 
-    public function logged() {
-        Authentication::getInstance()->setAuthenticated('loick111', 1);
-        $this->getView()->redirect('/');
+    public function logged() {}
+    public function loggedPOST() {
+        $this->getView()->ajax();
+
+        if(Input::post('username') == 'loick111' && Input::post('password') == 'azerty') {
+            Authentication::getInstance()->setAuthenticated('loick111', 1);
+            $res = [
+                'success' => true,
+                'display' => true,
+                'message' => 'Vous êtes maintenant connecté.'
+            ];
+        }else {
+            $res = [
+                'success' => false,
+                'display' => true,
+                'message' => 'La combinaison Nom d\'utilisateur/Mot de passe ne correspond pas.'
+            ];
+        }
+
+        echo json_encode($res);
     }
 
     public function logout() {
--- public/js/app.js
+++ public/js/app.js
@@ -1,6 +1,5 @@
 $(document).ready(function() {
-    addNews('loick111', 'Loïck Mahieux', '03/08/2015', '08:32', 'Contenu du message');
-    //addNews('loick111', 'Miléna Marchois', '05/04/2015', '10:26', "Je t'aime ! :D <3");
+    createNews('loick111', 'loick111@gmail.com', 'Loïck Mahieux', '03/08/2015', '08:32', 'Pellentesque vel bibendum mauris. Fusce volutpat dolor ut purus aliquet tristique. Aliquam rhoncus turpis sed eros euismod, vitae pretium sem interdum. Duis leo ligula, eleifend in risus ut, tempus congue sapien. Phasellus tincidunt varius odio a fermentum. Sed vel odio suscipit, ultricies diam at, egestas ex. Ut ut libero nec risus rhoncus lacinia. Quisque quis leo accumsan, tristique ipsum et, feugiat est. Etiam quam neque, tincidunt in odio a, rhoncus condimentum mi.');
 
     $('#connect').click(function() {
         window.location.replace("/users/logged")
@@ -10,7 +9,8 @@ $(document).ready(function() {
 });
 
 function start() {
-
+    checkRequired();
+    changeImgProfile();
     commentsDisplayToggle();
     commentsAddToggle();
 }
@@ -33,13 +33,13 @@ function commentsDisplayToggle() {
     })
 }
 
-function addNews(username, fullname, date, time, message) {
+function createNews(username, mail, fullname, date, time, message) {
     $('#loading-news').remove();
 
     $('#news')
         .prepend(
             $('<div>')
-                .attr('class', 'col-lg-8 col-lg-offset-2')
+                .attr('class', 'col-lg-6 col-lg-offset-3')
                 .append(
                     $('<div>')
                         .attr('class', 'panel panel-default')
@@ -54,7 +54,7 @@ function addNews(username, fullname, date, time, message) {
                                                 .attr('href', '/users/show/' + username)
                                                 .append(
                                                     $('<img>')
-                                                        .attr('src', '/public/img/users/' + username + '.png')
+                                                        .attr('src', 'http://gravatar.com/avatar/' + CryptoJS.MD5(mail))
                                                         .attr('alt', fullname)
                                                         .attr('class', 'img-circle profile')
                                                 )
@@ -142,3 +142,41 @@ function commentsAddToggle() {
         $(this).remove();
     });
 }
+
+function checkRequired() {
+    $('input[required]').focusout(function() {
+        if($(this).val() == '') {
+            $(this).parent()
+                .addClass('has-error');
+        } else {
+            $(this).parent()
+                .removeClass('has-error')
+                .addClass('has-success');
+        }
+    });
+}
+
+function changeImgProfile() {
+    $('input[name="mail"]').change(function() {
+        var hash = CryptoJS.MD5($('input[name="mail"]').val());
+
+        $('#img-profile').attr('src', 'http://gravatar.com/avatar/' + hash + '?s=150');
+    });
+}
+
+$('form').submit(function() {
+    var data = $(this).serialize();
+    $.ajax({
+        type: $(this).attr("method"),
+        url: $(this).attr("action"),
+        data: data,
+
+        success: function(data) {
+            if(data.display)
+                alert(data.message);
+            if(data.success)
+                window.location = '/';
+        }
+    });
+    return false;
+});
